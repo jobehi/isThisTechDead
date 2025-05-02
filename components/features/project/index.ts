b621882ea5e02396b5ab4eb4b10fafff6f8d684e
Youssef El Behi <youssef.elbehi@gmail.com>
2025-05-02 22:39:22 +0200

    feat: add project gallery

---
+++ components/features/project/index.ts
@@ -0,0 +1,9 @@
+/**
+ * Project Feature Components Index
+ *
+ * This file exports project-related feature components.
+ * Example usage: import { ProjectSubmissionForm, ProjectGallery } from '@/components/features';
+ */
+
+export { ProjectSubmissionForm } from './ProjectSubmissionForm';
+export { ProjectGallery } from './ProjectGallery';
