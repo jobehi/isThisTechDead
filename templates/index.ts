de90efd7f36a4eb9f15b8b9b9f7dce64a807f9e0
Youssef El Behi <youssef.elbehi@gmail.com>
2025-05-02 21:23:40 +0200

    feat: add global app layout

---
+++ templates/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Templates Index
+ *
+ * This file exports all template components that define the overall structure of pages.
+ * Templates are the highest level of the atomic design methodology and combine other components.
+ *
+ * Example usage: import { PageLayout } from '@/templates';
+ */
+
+export * from './PageLayout';
