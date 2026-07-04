06c47bd3657aa8ca7cf27c716b809a34815f7321
Youssef El Behi <youssef.elbehi@gmail.com>
2026-07-04 23:07:39 +0200

    fix: set base path to undefined when environment variable is root slash

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: process.env.SITE_URL || 'https://jobehi.github.io',
-  base: process.env.BASE_PATH || '/isThisTechDead',
+  base: process.env.BASE_PATH === '/' ? undefined : (process.env.BASE_PATH || '/isThisTechDead'),
   build: {
     assets: 'assets',
   },
